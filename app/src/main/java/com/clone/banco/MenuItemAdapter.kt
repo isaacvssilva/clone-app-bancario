c9245cfac2fd8a593b1960aa2561adde8582b5a5
Isaac <isaacvinicius2121@alu.ufc.br>
2021-10-23 17:34:57 -0300

    Criação de tela com ConstraintLayout, e compreendendo o uso do componente RecyclerView e seu Adapter.

---
+++ app/src/main/java/com/clone/banco/MenuItemAdapter.kt
@@ -0,0 +1,26 @@
+package com.clone.banco
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/*ViewHolder -> usado para gerenciar a posicao das listas*/
+class MenuItemAdapter : RecyclerView.Adapter<MenuItemAdapter.MenuItemAdapterViewHolder>() {
+
+    class MenuItemAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MenuItemAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
